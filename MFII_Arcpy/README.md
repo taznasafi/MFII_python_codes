f212865688300c3148ee256cf242cb375c8aa4ee
taznasafi <nasafi.taz@gmail.com>
2017-12-13 18:08:45 -0500

    Initial upload

---
+++ MFII_Arcpy/README.md
@@ -0,0 +1,1 @@
+# MFII_Arcpy
---
+++ MFII_Arcpy/path_links.py
@@ -0,0 +1,19 @@
+
+
+#basepath
+basepath = r"E:\MFII_Dec2016\GIS\Subsidy_information\WireCenter_Vintages"
+
+subsidy_table_path =r"E:\MFII_Dec2016\CSV Files\New_csv\CSV_TABLES.gdb\cetc_subsidy_pid_pivot"
+
+
+#state Fips csv
+Fips_table_path = r"E:\MFII_Dec2016\MFII_python_codes\CSVs\state FiPS.txt"
+
+# Number of LTE 5 providers per state
+LTE5_table_path = r"E:\MFII_Dec2016\MFII_python_codes\CSVs\LTE5_number_of_providers_per_state.csv"
+
+dba_provider_dec2016_path = r"E:\MFII_Dec2016\MFII_python_codes\CSVs\wtb_dba_provider_dec2016.csv"
+
+LTE5_gdb_path = r"E:\MFII_Dec2016\GIS\Dec2016_LTE_Coverage\f477_bd_LTE5_by_state_diss.gdb"
+
+wireCenter_fc_path = r"E:\MFII_Dec2016\GIS\Subsidy_information\WireCenter_Vintages\Wirecenter_working.gdb\_merged_wirecenters_pviot"
