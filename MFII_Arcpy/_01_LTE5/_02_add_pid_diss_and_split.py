0f9fb6dceca7e328eed62d82ad47f86138f33be2
taznasafi <nasafi.taz@gmail.com>
2017-12-30 15:43:58 -0500

    Update _02_add_pid_diss_and_split.py
    updated the gdb name links

--- MFII_Arcpy/_01_LTE5/_02_add_pid_diss_and_split.py
+++ MFII_Arcpy/_01_LTE5/_02_add_pid_diss_and_split.py
@@ -3,13 +3,13 @@ import os
 
 #ADD PID AND STATE FIPS to the cliped LTE 5
 pid = geotools.Tools()
-pid.inputGDB = path_links._03_gdb_name
+pid.inputGDB = path_links._03_gdb_name+".gdb"
 pid.attach_pid_StateFips_toCoverages()
 
 
 # Merge cliped LTE 5
 merge = geotools.Tools()
-merge.inputGDB = path_links.lte5clip_gdp_path
+merge.inputGDB = path_links._03_gdb_name+".gdb"
 merge.outputPathFolder = path_links.outputbasepath
 merge.outputGDBName = path_links._06_gdb_name
 merge.create_gdb()
@@ -21,7 +21,7 @@ merge.mergeCoverages()
 #dissolve by pid and state
 
 dissovle = geotools.Tools()
-dissovle.inputGDB = path_links.LTE5_merged_gdb_path
+dissovle.inputGDB = path_links._06_gdb_name+".gdb"
 dissovle.outputPathFolder = path_links.outputbasepath
 dissovle.outputGDBName = path_links._07_gdb_name
 dissovle.create_gdb()
@@ -33,8 +33,8 @@ dissovle.dissolveCoverages()
 #split LTE 5 by State and PID
 split_LTE5 = geotools.Tools()
 split_LTE5.outputPathFolder = path_links.outputbasepath
-split_LTE5.outputGDBName = "_04_split_LTE5_coverages"
-split_LTE5.inputGDB = path_links.LTE5_diss_gdb_path
+split_LTE5.outputGDBName = _08_gdb_name
+split_LTE5.inputGDB = path_links._07_gdb_name+".gdb"
 split_LTE5.create_gdb()
 split_LTE5.outputGDB = os.path.join(split_LTE5.outputPathFolder, split_LTE5.outputGDBName+".gdb")
 split_LTE5.splitCoverages(split_fields=['STATE_FIPS', "PID"])
