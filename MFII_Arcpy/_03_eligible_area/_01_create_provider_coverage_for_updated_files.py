bb4bf3b108ffc7ff88f5333836242a7a8927a6c1
taznasafi <nasafi.taz@gmail.com>
2018-02-20 11:59:07 -0500

    Add files via upload
    added scripts for the new shapefiles

--- MFII_Arcpy/_03_eligible_area/_01_create_provider_coverage_for_updated_files.py
+++ MFII_Arcpy/_03_eligible_area/_01_create_provider_coverage_for_updated_files.py
@@ -4,19 +4,20 @@ import os
 #intesect_ineligible area with grid
 
 intersect_coverages = geotools.Tools()
-intersect_coverages.outputPathFolder = path_links.inputbasepath
+intersect_coverages.outputPathFolder = r"D:\FCC_GIS_Projects\MFII\DataCollection\final_eligible_area\MFII_python_codes\Coverages\updated_files"
 intersect_coverages.outputGDBName = path_links.intersect_inelligible_coverages_gdb_name
 intersect_coverages.create_gdb()
-intersect_coverages.inputGDB = os.path.join(path_links.inputbasepath, path_links.mfi_blocks_erased_gdb_name+".gdb")
+intersect_coverages.inputGDB = os.path.join(r"D:\FCC_GIS_Projects\MFII\DataCollection\final_eligible_area\MFII_python_codes\Coverages\updated_files",
+                                            path_links.mfi_blocks_erased_gdb_name+".gdb")
 intersect_coverages.outputGDB = os.path.join(intersect_coverages.outputPathFolder,
                                              intersect_coverages.outputGDBName+".gdb")
-intersect_coverages.intersect_coverages_by_stateGrid(lte_output_table_folder_path=path_links.inputbasepath)
+intersect_coverages.intersect_coverages_by_stateGrid(lte_output_table_folder_path=r"D:\FCC_GIS_Projects\MFII\DataCollection\final_eligible_area\MFII_python_codes\Coverages\updated_files")
 
 
 # dissolve the by grid
 
 diss_coverages = geotools.Tools()
-diss_coverages.outputPathFolder = path_links.inputbasepath
+diss_coverages.outputPathFolder = r"D:\FCC_GIS_Projects\MFII\DataCollection\final_eligible_area\MFII_python_codes\Coverages\updated_files"
 diss_coverages.outputGDBName = path_links.diss_inelligible_by_grid_gdb_name
 diss_coverages.create_gdb()
 diss_coverages.inputGDB = intersect_coverages.outputGDB
@@ -27,13 +28,13 @@ diss_coverages.dissolve_ineligible_coverages()
 # erase water area
 
 erase_water_areas = geotools.Tools()
-erase_water_areas.outputPathFolder = path_links.inputbasepath
+erase_water_areas.outputPathFolder = r"D:\FCC_GIS_Projects\MFII\DataCollection\final_eligible_area\MFII_python_codes\Coverages\updated_files"
 erase_water_areas.outputGDBName = path_links.ineligible_area_with_wateronly
 erase_water_areas.create_gdb()
 erase_water_areas.outputGDB = os.path.join(erase_water_areas.outputPathFolder,
                                            erase_water_areas.outputGDBName+".gdb")
 erase_water_areas.inputGDB = diss_coverages.outputGDB
-erase_water_areas.erase_water_blocks_from_coverage(lte_table_output_folder=path_links.inputbasepath)
+erase_water_areas.erase_water_blocks_from_coverage(lte_table_output_folder=r"D:\FCC_GIS_Projects\MFII\DataCollection\final_eligible_area\MFII_python_codes\Coverages\updated_files")
 
 
 # add field
@@ -50,7 +51,7 @@ addfield.calculate_area_in_meters("AREA")
 
 deminimus = geotools.Tools()
 deminimus.inputGDB = erase_water_areas.outputGDB
-deminimus.outputPathFolder = path_links.outputbasepath
+deminimus.outputPathFolder = r"D:\FCC_GIS_Projects\MFII\DataCollection\final_eligible_area\MFII_python_codes\Coverages\updated_files\output"
 deminimus.outputGDBName = path_links.deminimus_ineligible_area_gdb_name
 deminimus.create_gdb()
 deminimus.outputGDB = os.path.join(deminimus.outputPathFolder, deminimus.outputGDBName+".gdb")
--- MFII_Arcpy/_03_eligible_area/_02_create_Eligible_area_for_updated_files.py
+++ MFII_Arcpy/_03_eligible_area/_02_create_Eligible_area_for_updated_files.py
@@ -4,17 +4,18 @@ import os
 # merge the coverages
 
 merge = geotools.Tools()
-merge.outputPathFolder = path_links.inputbasepath
+merge.outputPathFolder = r"D:\FCC_GIS_Projects\MFII\DataCollection\final_eligible_area\MFII_python_codes\Coverages\updated_files"
 merge.outputGDBName = "_merged_ineligible_area"
 merge.create_gdb()
-merge.inputGDB = os.path.join(path_links.outputbasepath, path_links.deminimus_ineligible_area_gdb_name+".gdb")
+merge.inputGDB = os.path.join(r"D:\FCC_GIS_Projects\MFII\DataCollection\final_eligible_area\MFII_python_codes\Coverages\updated_files\output",
+                              path_links.deminimus_ineligible_area_gdb_name+".gdb")
 merge.outputGDB = os.path.join(merge.outputPathFolder, merge.outputGDBName+".gdb")
 merge.merge_ineligible_coverages()
 
 # erase ineligible area from state grid
 
 erase = geotools.Tools()
-erase.outputPathFolder = path_links.inputbasepath
+erase.outputPathFolder = r"D:\FCC_GIS_Projects\MFII\DataCollection\final_eligible_area\MFII_python_codes\Coverages\updated_files"
 erase.outputGDBName = "_eligible_area"
 erase.create_gdb()
 erase.inputGDB = merge.outputGDB
@@ -24,7 +25,7 @@ erase.erase_coverages_from_state_boundary()
 # erase water area from eligible area
 
 erasewater = geotools.Tools()
-erasewater.outputPathFolder = path_links.outputbasepath
+erasewater.outputPathFolder = r"D:\FCC_GIS_Projects\MFII\DataCollection\final_eligible_area\MFII_python_codes\Coverages\updated_files\output"
 erasewater.outputGDBName = "_eligible_area_minus_water_area"
 erasewater.create_gdb()
 erasewater.outputGDB = os.path.join(erasewater.outputPathFolder, erasewater.outputGDBName+".gdb")
